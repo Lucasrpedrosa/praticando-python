ef189fbe27ab542822d720407dcb2b5cf4e178b7
Lucas Pedrosa <lucasrpedrosa25@gmail.com>
2026-03-18 22:50:23 -0300

    Declarando váriaveis

---
+++ cal.py
@@ -0,0 +1,17 @@
+nome = 'Lucas'
+idade = 17
+altura = 1.76
+peso_kg = 94.5
+
+print(f'Olá! me chamo {nome}, atualmente tenho {idade} anos, {altura} metros de altura e peso {peso_kg}kg.')
+
+print('====================================================================================')
+
+nome = str(input('Digite seu nome: '))
+idade = int(input('Digite sua idade: '))
+altura = float(input('Digite sua altura em metros: '))
+peso_kg = float(input('Digite seu peso em quilogramas: '))
+
+print(f'Seja bem vindo {nome}! vimos que você tem {idade} anos de idade, que sua altura é de {altura} metros e que pesa {peso_kg}kg.')
+
+print('=====================================================================================')
