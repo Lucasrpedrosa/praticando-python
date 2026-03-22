a1dcf20aaca93cc3d1dc24e02b11ba0a5c119f4a
Lucas Pedrosa <lucasrpedrosa25@gmail.com>
2026-03-22 14:53:02 -0300

    Adicionando uma breve calculadora de opedadores aritméticos

--- cal.py
+++ cal.py
@@ -1,3 +1,4 @@
+"""
 nome = 'Lucas'
 idade = 17
 altura = 1.76
@@ -7,14 +8,12 @@ print(f'Olá! me chamo {nome}, atualmente tenho {idade} anos, {altura} metros de
 
 print('====================================================================================')
 
-#nome = str(input('Digite seu nome: '))
-#idade = int(input('Digite sua idade: '))
-#altura = float(input('Digite sua altura em metros: '))
-#peso_kg = float(input('Digite seu peso em quilogramas: '))
+nome = str(input('Digite seu nome: '))
+idade = int(input('Digite sua idade: '))
+altura = float(input('Digite sua altura em metros: '))
+peso_kg = float(input('Digite seu peso em quilogramas: '))
 
-#print(f'Seja bem vindo {nome}! vimos que você tem {idade} anos de idade, que sua altura é de {altura} metros e que pesa {peso_kg}kg.')
-
-print('=====================================================================================')
+print(f'Seja bem vindo {nome}! vimos que você tem {idade} anos de idade, que sua altura é de {altura} metros e que pesa {peso_kg}kg.')
 
 print('Operações aritméticas')
 print('Operação de soma: 5 + 5 = ', 5 + 5)
@@ -24,3 +23,41 @@ print('Operação de divisão (resto float): 5 / 5 = ', 5 / 5)
 print('Operação de divisão (resto inteiro): 5 // 5 = ', 5 // 5)
 print('Operação de módulo: 5 % 5 = ', 5 % 5)
 print('Operação de exponenciação: 5 ** 5 = ', 5 ** 5)
+"""
+print('Olá, Seja Bem Vindo(a) a um breve programa de calculadora de operadores aritméticos!')
+
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite outro número: '))
+
+print('Informe o número de uma das seguintes operações:')
+print('[1] Soma')
+print('[2] Subtração')
+print('[3] Multiplicação')
+print('[4] Divisão')
+print('[5] Divisão (resto inteiro)')
+print('[6] Potência')
+
+operacao = str(input('Digite o número da operação desejada: '))
+
+if operacao == '1':
+    print(f'A operação escolhida foi Soma! O resultado de {n1} + {n2} = {n1+n2}')
+
+elif operacao == '2':
+    print(f'A operação escolhida foi Subtração! O resultado de  {n1} - {n2} = {n1-n2}')
+
+elif operacao == '3':
+    print(f'A operação escolhida foi Multiplicação! O resultado de {n1} * {n2} = {n1*n2}')
+
+elif operacao == '4':
+    print(f'A operação escolhida foi Divisão! O resultado de {n1} / {n2} = {n1/n2}')
+
+elif operacao == '5':
+    print(f'A operação escolhida foi Divisão (resto inteiro)! O resultado de {n1} // {n2} = {n1//n2}')
+
+elif operacao == '6':
+    print(f'A operação escolhida foi Potência! O resultado de {n1} ** {n2} = {n1**n2}')
+
+else:
+    print('Ocorreu um erro!!!')
+
+    
