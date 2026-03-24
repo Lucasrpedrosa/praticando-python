d32d90d09bf903afed931d2f77dfbd64aa17aded
Lucas Pedrosa <lucasrpedrosa25@gmail.com>
2026-03-23 23:08:35 -0300

    Corrigindo erros na calculadora de operações para que informe caso o jogador tenha acertado o número e imprimir qual número ele acertou + arquivo (pratica.py) para praticar outras funções

--- cal.py
+++ cal.py
@@ -5,13 +5,10 @@ print('Olá, Bem Vindo(a) ao teste de operadores de comparação e atribuição!
 print('O programa irá funcionar da seguinte forma: O computador ira selecionar uma número de 1 a 10, sua missão é adivinhar qual é o valor exato, mas caso não consiga acertar de primeira,  o computador ira informar e dar dicas se o valor selecionado por você é maior ou menor que o valor padrão. Em seguida pedirá para informar qual operação e qual valor vc escolherá para tentar novamente acertar o valor da máquina.')
 
 n1 = random.randint(1,10)
-n2 = 0
+n2 = int(input('Adivinhe qual número inteiro a máquina está pensando: '))
 while n2 != n1:
-    n2 = int(input('Adivinhe qual número inteiro a máquina está pensando: '))
-    if n2 == n1:
-        print('Parabéns! Você acertou!')
-        break
-    elif n2 < n1:
+    
+    if n2 < n1:
         continua = str(input('Passou perto! Seu valor selecionado é menor que o da máquina, deseja continuar?(S/N) '))
         if continua == 'N':
             break
@@ -46,8 +43,10 @@ while n2 != n1:
     else:
         print('Erro Geral!')
     
-    
-    
+    n2 = int(input('Tente adivinhar novamente: '))
+
+if n2 == n1:
+        print(f'Parabéns, você acertou! o número selecionado foi {n2}') 
 
 
 
---
+++ pratica.py
@@ -0,0 +1,2 @@
+
+pergunta = int(input('Diga uma operação: \n [1] Somar \n [2] Subrair \n'))
