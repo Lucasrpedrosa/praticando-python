5661fd9b4dec77996f95359e18d6a4d8adde4fd0
Lucas Pedrosa <lucasrpedrosa25@gmail.com>
2026-03-24 23:05:11 -0300

    Adicionando leitura de nome do cliente(nome e sobrenome), leitura de idade e tipo de cliente

--- Verifi_contas_banco.py
+++ Verifi_contas_banco.py
@@ -1,4 +1,4 @@
-print(f' Seja bem vindo(a) a verificação de contas do Banco do Brasil! \n Neste programa você poderá verificar as seguintes operações: \n - Verificar saldo; \n  - Deposito direto e/ou poupança; \n  - Saque direto e/ou poupança. \n ===== Exclusivo (Aposentados) ===== \n  - Saque do FGTS; \n ===== Exclusivo (Juvenis) ===== \n  - Limite Reduzido de saques e depósitos (Sem acesso a poupança) ')
+print(f' Seja bem vindo(a) a verificação de contas do Banco Venturis! \n Neste programa você poderá verificar as seguintes operações: \n - Verificar saldo; \n  - Deposito direto e/ou poupança; \n  - Saque direto e/ou poupança. \n ===== Exclusivo (Aposentados) ===== \n  - Saque do FGTS; \n ===== Exclusivo (Juvenis) ===== \n  - Limite Reduzido de saques e depósitos (Sem acesso a poupança) ')
 
 primeiro_nome_cliente = str(input('Para começar, digite seu primeiro nome: '))
 sobrenome_cliente = str(input('Digite seu sobrenome: '))
@@ -6,4 +6,5 @@ idade_cliente = int(input('Diga sua data de nascimento (Ex: dd/mm/aa): '))
 while True:
     tipo_cliente = str(input(f'Olá {primeiro_nome_cliente}, informe-nos que tipo de cliente você se encaixa: \n [P] Padrão \n [A] Aposentado \n [J] Juvenil'))
     if tipo_cliente == 'P' or tipo_cliente == 'p':
-        saldo_cliente_padrao = int(input('Ótimo, você já esta cadastrado como conta Padrão!'))
+        resposta = int(input('Ótimo, você já esta cadastrado como conta Padrão! Deseja realizar seu primeiro depósito? [S] Sim / [N] Não: '))
+        
