ed9e530463e69b26e6539ad4e51e1dff62cbf6fe
Lucas Pedrosa <lucasrpedrosa25@gmail.com>
2026-03-27 18:32:36 -0300

    Adicionando pasta para anotações sobre python + retomando programa de verificação de contas.

--- Verifi_contas_banco.py
+++ Verifi_contas_banco.py
@@ -1,7 +1,28 @@
-print(f' Seja bem vindo(a) a verificação de contas do Banco Venturis! \n Neste programa você poderá verificar as seguintes operações: \n - Verificar saldo; \n  - Deposito direto e/ou poupança; \n  - Saque direto e/ou poupança. \n ===== Exclusivo (Aposentados) ===== \n  - Saque do FGTS; \n ===== Exclusivo (Juvenis) ===== \n  - Limite Reduzido de saques e depósitos (Sem acesso a poupança) ')
+print(''' 
+      ===== Seja bem vindo(a) a verificação de contas do Banco Venturis! =====
+      
+      Neste programa você poderá verificar as seguintes operações:  
+        - Verificar saldo;
+        - Deposito direto e/ou poupança; 
+        - Saque direto e/ou poupança. 
+      
+      ===== Exclusivo (Aposentados) ===== 
+        - Saque do FGTS; 
+      
+      ===== Exclusivo (Juvenis) ===== 
+        - Limite Reduzido de saques e depósitos (Sem acesso a poupança) 
+      ''')
+
+lista_nome = []
+lista_email = []
+matriz_transacoes =  []
 
 primeiro_nome_cliente = str(input('Para começar, digite seu primeiro nome: '))
+
 sobrenome_cliente = str(input('Digite seu sobrenome: '))
+
+
+
 idade_cliente = int(input('Diga sua data de nascimento (Ex: dd/mm/aa): '))
 while True:
     tipo_cliente = str(input(f'Olá {primeiro_nome_cliente}, informe-nos que tipo de cliente você se encaixa: \n [P] Padrão \n [A] Aposentado \n [J] Juvenil'))
