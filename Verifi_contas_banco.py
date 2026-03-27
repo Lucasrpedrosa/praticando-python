5c56f3be9e69cdb02886cc5af259185ef1d2be9e
Lucas Pedrosa <lucasrpedrosa25@gmail.com>
2026-03-27 20:09:15 -0300

    Inplementando condicionais e cadastro do cliente incluindo, nome e sobrenome, email e senha ( confirmação de se senha em loop, corrigir )

--- Verifi_contas_banco.py
+++ Verifi_contas_banco.py
@@ -15,17 +15,34 @@ print('''
 
 lista_nome = []
 lista_email = []
+lista_senhas = []
 matriz_transacoes =  []
 
-primeiro_nome_cliente = str(input('Para começar, digite seu primeiro nome: '))
 
-sobrenome_cliente = str(input('Digite seu sobrenome: '))
-
-
-
-idade_cliente = int(input('Diga sua data de nascimento (Ex: dd/mm/aa): '))
-while True:
-    tipo_cliente = str(input(f'Olá {primeiro_nome_cliente}, informe-nos que tipo de cliente você se encaixa: \n [P] Padrão \n [A] Aposentado \n [J] Juvenil'))
-    if tipo_cliente == 'P' or tipo_cliente == 'p':
-        resposta = int(input('Ótimo, você já esta cadastrado como conta Padrão! Deseja realizar seu primeiro depósito? [S] Sim / [N] Não: '))
+cadastro_cliente = str(input('Para começar, diga-nos se você já possui cadastro no Banco Venturis? [S] Sim / [N] Não: '))
+if cadastro_cliente == 'N' or cadastro_cliente == 'n':
+  while True:
         
+    primeiro_nome_cliente = str(input('Para começar, digite seu primeiro nome: '))
+    sobrenome_cliente = str(input('Digite seu sobrenome: '))
+    lista_nome.extend(f'{primeiro_nome_cliente} {sobrenome_cliente}')
+        
+    email_cliente = str(input('Informe-nos seu email: '))
+    if '@gmail.com' not in email_cliente:
+      lista_email.append(f'{email_cliente}@gmail.com')
+
+    elif '@gmail.com' in email_cliente:
+      lista_email.append(email_cliente)
+
+    else:
+      print('Email invalálido!')
+      continue
+      
+    senha_cliente = str(input('Digite sua senha: '))
+    confirm_senha_cliente = str(input('Confirme sua senha: '))
+    while confirm_senha_cliente != senha_cliente:
+      print('Valor inváido, digite novamente!')
+    
+    if confirm_senha_cliente == senha_cliente: 
+      print('Senha cadastrada com sucesso!')
+      lista_senhas.append(senha_cliente)
