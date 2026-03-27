f9a5f329f1d01fbec97bd6a8c302cfc51c71c6d4
Lucas Pedrosa <lucasrpedrosa25@gmail.com>
2026-03-27 16:01:29 -0300

    Adcionando anotações detalhadas sobre métodos de listas (append, clear, count, extend, index, pop, remove, len, reverse, sort)

--- listas.py
+++ listas.py
@@ -1,6 +1,6 @@
 # Lista padrão
 
-lista_nome = ["Lucas", "Vitor", "Gabriel"]
+lista_nome = ["Lucas", "Vitor", "Gabriel", "Lucas"]
 
 # Lista vazia
 
@@ -80,5 +80,80 @@ for indice, carro in enumerate(carros):
     # Exemplo reduzido
 
     quadrado = [valor ** 2 for valor in valores]
+
+
+    # Métodos para listas
+
+    #.append() -  adiciona objetos a lista
+
+    lista = []
+
+    lista.append(1)
+    lista.append("Python")
+    lista.append([40, 30, 20])
+
+    print(lista)  # [1, "Python", [40, 30, 20]]
+    
+    # .clear() - limpa a lista
+    lista.clear()
+    print(lista) # []
+
+    # .copy() - copia a lista
+    lista_nome_copy = lista_nome.copy()
+    print(lista_nome)   # ["Lucas", "Vitor", "Gabriel", "Lucas"]
+    print(lista_nome_copy) # ["Lucas", "Vitor", "Gabriel", "Lucas"]
+
+    # .count() - Conta quantos objetos X tem na lista
+
+    lista_nome.count("Vitor") # 1
+    lista_nome.count("Lucas") # 2
+
+    # .extend - Adiciona mais de um objeto de uma vez a lista
+
+    lista.extend([1, "LUCAS", 89, 100])
+    print(lista) # [1, "LUCAS", 89, 100]
+
+    # .index() - Exibe a primeira posição em que o objeto está 
+
+    lista_nome.index("Lucas") # 0
+    lista_nome.index("Vitor") # 1
+
+    # .pop() - Remove objetos da lista pelo indice, caso não especifique remove o último adicionado
+
+    lista.pop() # 100
+    lista.pop(0) # 1
+
+    # .remove() - Remove o objeto da lista
+
+    lista.remove("LUCAS")
+    print(lista) # [89]
+
+    # .reverse() - Inverte a ordem da lista
+
+    lista_a = ["lucas", "java", "oi"]
+    lista_a.reverse()
+    print(lista_a)  # ["oi", "java", "lucas"]
+
+    # len - Exibe quantos objetos tem na lista
+
+    print(len(lista_a)) # 3
+
+    #.sort() - ordena a lista
+
+    linguagens = ["python", "js", "c", "java", "csharp"]
+    linguagens.sort()  # ["c", "csharp", "java", "js", "python"] - Ordem alfabetica
+    print(linguagens)
+
+    linguagens = ["python", "js", "c", "java", "csharp"]
+    linguagens.sort(reverse=True)  # ["python", "js", "java", "csharp", "c"] - Inverte a ordem
+    print(linguagens)
+
+    linguagens = ["python", "js", "c", "java", "csharp"]
+    linguagens.sort(key=lambda x: len(x))  # ["c", "js", "java", "python", "csharp"] - Ordem Crescente
+    print(linguagens)
+
+    linguagens = ["python", "js", "c", "java", "csharp"]
+    linguagens.sort(key=lambda x: len(x), reverse=True)  # ["python", "csharp", "java", "js", "c"] - Ordem decrescente
+    print(linguagens)
 
 
